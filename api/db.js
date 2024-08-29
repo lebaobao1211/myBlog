ceff5785aa12a88a3bad85441b4f99bb79da12c3
lebaobao1211 <lebaobao4869@gmail.com>
2024-08-29 13:44:08 +0700

    last commit

---
+++ api/db.js
@@ -0,0 +1,14 @@
+import mysql from "mysql2";
+
+export const db= mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"123456",
+    database:"blog"
+
+})
+
+    
+        
+
+    
